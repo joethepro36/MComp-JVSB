6a637253f51c5003ba8b40f22f61b6df2bbce601
bobbybrot <bobby.brot@hotmail.co.uk>
2017-09-20 20:58:00 +0100

    Uploaded Hello world program

---
+++ c++ Tester/c++ Tester/cTester.cpp
@@ -0,0 +1,12 @@
+// cTester.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello world";
+    return 0;
+}
+
